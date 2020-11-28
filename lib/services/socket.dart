f8aa57349294caa26705da0468f89c343efebc62
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2020-11-28 20:55:07 +0100

    CHANGE: Metemos socketIO y Provider para notificar del status de la conexión entre el Backend y la APP con un notifyListeners de Provider.

---
+++ lib/services/socket.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus {
+  Online,
+  Offline,
+  Connecting,
+}
+
+class SocketService with ChangeNotifier {
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+  get serverStatus => this._serverStatus;
+
+  SocketService() {
+    this._initConfig();
+  }
+
+  void _initConfig() {
+    // Dart client
+    IO.Socket socket = IO.io('http://192.168.1.33:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': true
+    });
+
+    socket.on('connect', (_) {
+      print('Connected');
+      this._serverStatus = ServerStatus.Online;
+      notifyListeners();
+    });
+
+    socket.on('disconnect', (_) {
+      print('Disconnected');
+      this._serverStatus = ServerStatus.Offline;
+      notifyListeners();
+    });
+  }
+}
